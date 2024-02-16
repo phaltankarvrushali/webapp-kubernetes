ec12e89a159d709e7fec8a2db8fd89dfd3471dbc
Vrushali Phaltankar <phaltankarvrushali@gmail.com>
2024-02-15 20:19:14 -0500

    Updated app.js

--- webapp/app.js
+++ webapp/app.js
@@ -1,26 +1,18 @@
-// var express = require('express');
-
-// var app = express();
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
-
-
 var express = require('express');
 var app = express();
 
-app.use(function (req, res, next) {
-  console.log('Request received:', req.method, req.url);
-  next();
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
 });
 
 app.get('/', function (req, res) {
-  res.send('Hi there, I am Vrushali Phaltankar');
+  res.render('index');
 });
 
 app.get('/about', function (req, res) {
@@ -31,15 +23,14 @@ app.get('/contact', function (req, res) {
   res.send('Contact Page');
 });
 
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+app.get('/portfolio', function (req, res) {
+  res.send('Our portfolio showcases our recent projects.');
 });
 
 app.use(function (req, res, next) {
   res.status(404).send("Sorry, can't find that!");
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3002, function () {
+  console.log('Example app listening on port 3002!');
 });
